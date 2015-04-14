ecbe678d59178b45f75f4e1450f2947e7578f3a5
MCSpark <riapov.evgeny@gmail.com>
2015-04-14 10:13:04 +0300

    Update _assign.php

--- views/doctor/_assign.php
+++ views/doctor/_assign.php
@@ -14,7 +14,11 @@ use yii\helpers\ArrayHelper;
 
     <?php $form = ActiveForm::begin(); ?>
 
-        <?= $form->field($model, 'patients')->dropDownList(ArrayHelper::map($model->selectPatients, 'id', 'full_name' ), ['size'=>10, 'multiple' => true])->label(false) ?>
+        <div class="well well-sm">
+            <fieldset style="height: 200px; overflow: auto">
+                <?= $form->field($model, 'patients')->checkboxList(ArrayHelper::map($model->selectPatients, 'id', 'full_name' ), ['separator' => '<br>'])->label(false) ?>
+            </fieldset>
+        </div>
         <div class="form-group">
             <?= Html::submitButton(Yii::t('app', 'Update'), ['class' => 'btn btn-success']) ?>
             <?= Html::a(Yii::t('app', 'Cancel'), ['/doctor/view', 'id' => $model->id], ['class' => 'btn btn-danger']) ?>
